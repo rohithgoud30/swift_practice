cdfac85875649e3a04ef24dc3803490ce8c946bb
rohithgoud30 <rohithgoud30@gmail.com>
2023-12-15 20:47:10 -0500

    Updated Functions.playground

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,7 +1,22 @@
 import Foundation
 
-let sayHello={(name:String)->String in
-    "Hello, \(name)"
+func multiply(num a: Int,with b:Int){
+    a*b
 }
 
-sayHello("Rohith")
+multiply(num: 6, with: 2)
+
+func getFullName(_ firstName:String="Foo",_ lastName:String="Bar")->String{
+    "\(firstName) \(lastName)"
+}
+
+getFullName()
+getFullName("Rohith")
+getFullName("Rohith","Goud")
+
+func getFullName1(firstName:String="Foo",lastName:String="Bar")->String{
+    "\(firstName) \(lastName)"
+}
+
+getFullName1(firstName: "Rohith",lastName: "Goud")
+getFullName1(lastName:"Goud")
