4d2a78f0f783f6c9851b1bf137e88880a20affa2
rohithgoud30 <rohithgoud30@gmail.com>
2023-12-15 21:14:11 -0500

    Updated Clousers.playground

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -1,5 +1,17 @@
 import Foundation
 
+let sayHello={
+    "Hello Rohith"
+}
+
+sayHello()
+
+let sayHello1={ (name:String)->String in
+    "Hello \(name)"
+}
+
+sayHello1("Rohith")
+
 func funcWithParams(_ name:String,_ age:Int,_ email:String,param:(String,Int,String)->Void){
     param(name,age,email)
 }
@@ -19,19 +31,46 @@ func customAddTwo1(_ lhs:Int,_ rhs:Int, using function:(Int,Int)->Int)->Int{
     function(lhs,rhs)
 }
 
-customAddTwo1(20, 10,using:{(lhs:Int,rhs:Int)->Int in
+customAddTwo1(
+    20,
+    10,
+    using:{(lhs:Int,rhs:Int)->Int in
     lhs + rhs
 })
 
-customAddTwo1(20, 10){
+customAddTwo1(
+    20,
+    10){
     (lhs:Int,rhs:Int)->Int in
     lhs + rhs
 }
 
-func customAddTwo(_ lhs:Int,_ rhs:Int, using function:(Int,Int)->Int)->Int{
+func customAddTwo(
+    _ lhs:Int,
+    _ rhs:Int,
+    using function:(Int,Int)->Int)->Int{
     function(lhs,rhs)
 }
 
-customAddTwo(20, 10){
+customAddTwo(
+    20,
+    10
+){
     $0 + $1
 }
+
+
+func add10(_ value:Int)->Int{
+    value+10
+}
+
+func add20(_ value:Int)->Int{
+    value+20
+}
+
+func doAddition(on value: Int,using function:(Int)->Int)->Int{
+    function(value)
+}
+
+print(doAddition(on: 20){add10($0)})
+
