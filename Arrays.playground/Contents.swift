989048c09bc594112b6867a41f65604aa4432b58
rohithgoud30 <rohithgoud30@gmail.com>
2024-02-22 16:57:28 -0500

    Initial Commit

---
+++ Arrays.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var arr:[Int]=[1,2,3,4,5,6]
+
+//arr.remove(at: 1)
+//arr.shuffle()
+//arr.insert(12, at: 0)
+//arr.count
+//arr.last
+//arr.first
+//arr[1]
+print(arr)
